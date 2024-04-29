a726089e93fe29570523ead6d60ad6d19c471f93
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2024-04-29 15:48:01 +0000

    navbar and tabsview changes commited

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Home</span>
-			</Link>
-			
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar navbar-dark bg-custom">
+            <Link to="/">
+                <span className="navbar-brand mb-0 h1">Home</span>
+            </Link>
+            
+            <div className="ml-auto">
+                <Link to="/demo">
+                    <button className="btn btn-primary">Check the Context in action</button>
+                </Link>
+            </div>
+        </nav>
+    );
 };
+
--- src/js/component/tabs.js
+++ src/js/component/tabs.js
@@ -1,20 +1,19 @@
 import React, { useEffect, useContext, } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import "../../styles/tabs.css";
 
 export const Tabviews = () => {
-    // const navigate = useNavigate();
-  
-    return (
-      <div>
-        <Link to = "/qualies">     
-            <button id="qualifier-results-btn">Qualifier Results</button>
-        </Link> 
-        <Link to = "/raceresults">  
-          <button id="race-results-btn">Race Results</button>
-        </Link>
+  return (
+      <div className="tab-container">
+          <Link to="/qualies">
+              <button id="qualifier-results-btn">Qualifier Results</button>
+          </Link>
+          <Link to="/raceresults">
+              <button id="race-results-btn">Race Results</button>
+          </Link>
       </div>
-    );
-  };
-  
-  export default Tabviews;
+  );
+};
+
+export default Tabviews;
