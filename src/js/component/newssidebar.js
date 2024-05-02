6437b88f6983b9d196d86b24708763b0794c48ac
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2024-05-02 14:26:56 +0000

    newssidebar has been properly formatted

--- src/js/component/newssidebar.js
+++ src/js/component/newssidebar.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { Context } from "../store/appContext";
+import "../../styles/newsSidebar.css"; // Import CSS file for styling
 
 const NewsSidebar = () => {
     const { store, actions } = useContext(Context); // Access store and actions from context
@@ -11,12 +12,13 @@ const NewsSidebar = () => {
     return (
         <div className="news-sidebar">
             <h2>Top Formula 1 News</h2>
-            <ul>
+            <ul className="article-list">
                 {store.topF1News.map((article, index) => (
                     <li key={index}>
-                        <h3>{article.title}</h3>
-                        <p>{article.description}</p>
-                        <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
+                        <a href={article.link} target="_blank" rel="noopener noreferrer" className="article-link">
+                            <h5>{article.title}</h5>
+                            {article.image_url && <img src={article.image_url} alt={article.title} style={{ width: '150px', height: 'auto' }} />}
+                        </a>
                     </li>
                 ))}
             </ul>
@@ -25,4 +27,6 @@ const NewsSidebar = () => {
 };
 
 export default NewsSidebar;
+
+
 
